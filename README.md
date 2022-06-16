d6d4c0486a54c809951d9fddfe5b4aa8f1580098
martinlie <martin.lie@gmail.com>
2022-06-16 14:02:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-jbe5w9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-jbe5w9)
---
+++ index.js
@@ -0,0 +1,6 @@
+// run `node index.js` in the terminal
+
+import * as tf from '@tensorflow/tfjs';
+
+
+console.log(`Hello Node.js v${process.versions.node}!`);
